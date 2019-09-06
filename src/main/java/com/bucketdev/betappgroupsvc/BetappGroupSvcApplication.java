cb96f465df96ac3c53dea22d159730c55cb42fd8
vanfigo <rodrigo.loyola@live.com>
2019-09-06 17:06:45 -0500

    first commit

---
+++ src/main/java/com/bucketdev/betappgroupsvc/BetappGroupSvcApplication.java
@@ -0,0 +1,13 @@
+package com.bucketdev.betappgroupsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetappGroupSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetappGroupSvcApplication.class, args);
+	}
+
+}
